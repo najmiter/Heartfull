e53660e40e084170fc933fd0f7c298a2643af245
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-10 22:18:28 +0500

    saving it to the local storage

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ fetch("tasbih.json")
         const today = new Date();
         const day = today.getDay();
 
-        let counter_count = 0;
+        let counter_count = +localStorage.getItem("counter") ?? 0;
         const target = 100;
 
         const qalma = document.getElementById("qalma");
@@ -15,6 +15,7 @@ fetch("tasbih.json")
         qalma.setAttribute("src", `imgs/${tasbihaat[day].day}.svg`);
         greeting.textContent = `Target: ${target}`;
         counter_text.textContent = counter_count;
+        loop.textContent = `Loop: ${localStorage.getItem("loop") ?? 0}`;
 
         document
             .getElementsByTagName("main")[0]
@@ -22,13 +23,18 @@ fetch("tasbih.json")
 
         function set_counter_count() {
             counter_count++;
-            const remaining = counter_count % target;
-            counter_text.textContent = remaining ? remaining : target;
+            const remaining =
+                counter_count % target ? counter_count % target : target;
+            counter_text.textContent = remaining;
 
             if (counter_count % target === 0) set_loop();
+
+            localStorage.setItem("counter", remaining);
         }
 
         function set_loop() {
-            loop.textContent = `Loop: ${Math.trunc(counter_count / target)}`;
+            const loop_count = Math.trunc(counter_count / target);
+            loop.textContent = `Loop: ${loop_count}`;
+            localStorage.setItem("loop", loop_count);
         }
     });
