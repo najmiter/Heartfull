6c7603461be230005d6dde6085ff8896c229e9aa
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-10 21:46:58 +0500

    And so it begins

---
+++ main.js
@@ -0,0 +1,34 @@
+fetch("tasbih.json")
+    .then((res) => res.json())
+    .then((tasbihaat) => {
+        const today = new Date();
+        const day = today.getDay();
+
+        let counter_count = 0;
+        const target = 10;
+
+        const qalma = document.getElementById("qalma");
+        const greeting = document.getElementById("greeting");
+        const counter = document.getElementById("counter");
+        const counter_text = document.getElementById("counter-text");
+        const loop = document.getElementById("loop");
+
+        qalma.setAttribute("src", `imgs/${tasbihaat[day].day}.svg`);
+        greeting.textContent = `Target: ${target}`;
+        counter_text.textContent = counter_count;
+
+        counter.addEventListener("click", set_counter_count);
+
+        function set_counter_count() {
+            counter_count++;
+            const remaining = counter_count % (target + 1);
+            counter_text.textContent = remaining;
+            // greeting.textContent = `Target: ${target - remaining}`;
+
+            if (counter_count % target === 0) set_loop();
+        }
+
+        function set_loop() {
+            loop.textContent = `Loop: ${Math.trunc(counter_count / target)}`;
+        }
+    });
